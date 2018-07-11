4f1a72b709f5bd05bb3628cbd84855d6c290078d
Joffrey Janiec <jjaniec@student.42.fr>
2018-07-11 12:35:22 +0200

    add docker_deploy playbooks

--- sandbox/init_training_containers.sh
+++ sandbox/init_training_containers.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 ansible-playbook ../playbooks/pip_docker_module.yml && \
-ansible-playbook ../playbooks/docker_pull_ubuntu.yml && \
-ansible-playbook ../playbooks/docker_pull_nginx.yml && \
-ansible-playbook ../playbooks/docker_pull_jenkins.yml || exit 1
-
-ansible-playbook start_training_containers.yml
+ansible-playbook ../playbooks/docker_deploy_ubuntu.yml && \
+ansible-playbook ../playbooks/docker_deploy_nginx.yml && \
+ansible-playbook ../playbooks/docker_deploy_jenkins.yml
