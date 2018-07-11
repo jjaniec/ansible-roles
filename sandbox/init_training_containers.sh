0ab19e39bde0dd35f6dfef0831ee80a05c50dcf9
Joffrey Janiec <jjaniec@student.42.fr>
2018-07-11 14:08:25 +0200

    use playbook instead of script to create sandbox containers

--- sandbox/init_training_containers.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ansible-playbook ../playbooks/pip_docker_module.yml && \
-ansible-playbook ../playbooks/docker_deploy_ubuntu.yml && \
-ansible-playbook ../playbooks/docker_deploy_nginx.yml && \
-ansible-playbook ../playbooks/docker_deploy_jenkins.yml
